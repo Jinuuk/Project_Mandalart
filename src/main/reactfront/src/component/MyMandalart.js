8b4baf95920715ecff2bb23cb3b0e54233c1d36f
Jinuk Kim <zpd29thd03@naver.com>
2022-11-11 20:10:09 +0900

    메인화면 그리드 적용

--- src/main/reactfront/src/component/MyMandalart.js
+++ src/main/reactfront/src/component/MyMandalart.js
@@ -1,11 +1,58 @@
 function MyMandalart() {
   return(
     <div>
-      <div>나의 만다라트</div>
-      <div>목록</div>
-      <div>만다라트 작성하기</div>
+      <section>누구누구님의 만다라트</section>
+      <section>
+        <div className="mandalart-table">
+          <div className="main-item m1">
+            <DetailItem/>
+          </div>
+          <div className="main-item m2">
+            <DetailItem/>
+          </div>
+          <div className="main-item m3">
+           <DetailItem/>
+          </div>
+          <div className="main-item m4">
+           <DetailItem/>
+          </div>
+          <div className="main-item m5">
+            <DetailItem/>
+          </div>
+          <div className="main-item m6">
+            <DetailItem/>
+          </div>
+          <div className="main-item m7">
+            <DetailItem/>
+          </div>
+          <div className="main-item m8">
+          <DetailItem/>
+          </div>
+          <div className="main-item m9">
+            <DetailItem/>
+          </div>
+        </div>
+      </section>
+      <section>section2</section>
     </div>
   )
 }
+
+function DetailItem() {
+  return (
+    <>
+      <textarea className="detail-item d1"></textarea>
+      <textarea className="detail-item d2"></textarea>
+      <textarea className="detail-item d3"></textarea>
+      <textarea className="detail-item d4"></textarea>
+      <textarea className="detail-item d5"></textarea>
+      <textarea className="detail-item d6"></textarea>
+      <textarea className="detail-item d7"></textarea>
+      <textarea className="detail-item d8"></textarea>
+      <textarea className="detail-item d9"></textarea>
+    </>
+  )
+}
+
 
 export default MyMandalart;
